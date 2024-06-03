6d8401fbe6f2cd4047fd2582fda3d79663b51de3
Gloria-Kaloli <gloriakaloli00@gmail.com>
2024-06-03 15:30:21 -0700

    HEIGHT

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,11 +1,12 @@
 #include "binary_trees.h"
 size_t recurse_for_height(const binary_tree_t *tree);
 /**
- * binary_tree_height - measure height of tree
+ * binary_tree_height - Function that measures height of tree
  * @tree: tree to measure
  *
  * Return: height
  */
+
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (!tree)
@@ -13,12 +14,14 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	return (recurse_for_height(tree) - 1);
 }
+
 /**
- * recurse_for_height - measure height
+ * recurse_for_height - Function that measure height
  * @tree: tree to measure
  *
  * Return: height
  */
+
 size_t recurse_for_height(const binary_tree_t *tree)
 {
 	size_t heightR, heightL;
