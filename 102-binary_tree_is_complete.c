64dcf69b0ab8eb33397a4263af19e326df5d5654
Gloria-Kaloli <gloriakaloli00@gmail.com>
2024-06-03 16:23:02 -0700

    COPMLETE

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -2,11 +2,12 @@
 int recurse_for_complete(const binary_tree_t *tree, size_t index, size_t size);
 size_t binary_tree_size(const binary_tree_t *tree);
 /**
- * binary_tree_is_complete - check if tree is complete
+ * binary_tree_is_complete - Functio to check if tree is complete
  * @tree: pointer to root of tree
  *
  * Return: 1 if complete; 0 if incomplete
  */
+
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
 	if (!tree)
@@ -14,14 +15,16 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 
 	return (recurse_for_complete(tree, 0, binary_tree_size(tree)));
 }
+
 /**
- * recurse_for_complete - use array indexing to track node position
+ * recurse_for_complete - Function to use array indexing to track node position
  * @tree: pointer to root of tree
  * @index: index of node in conceptual array
  * @size: number of nodes in tree
  *
  * Return: 1 if complete; 0 if incomplete
  */
+
 int recurse_for_complete(const binary_tree_t *tree, size_t index, size_t size)
 {
 	int left, right;
@@ -40,12 +43,14 @@ int recurse_for_complete(const binary_tree_t *tree, size_t index, size_t size)
 	else
 		return (0);
 }
+
 /**
- * binary_tree_size - measure size of binary tree
+ * binary_tree_size - Function to measure size of binary tree
  * @tree: tree to measure
  *
  * Return: size
  */
+
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (tree)
