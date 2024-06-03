08e4204115440059cfd540c4abaa06a09bc6c98c
Gloria-Kaloli <gloriakaloli00@gmail.com>
2024-06-03 16:44:18 -0700

    BST-SERACH

--- 113-bst_search.c
+++ 113-bst_search.c
@@ -1,13 +1,14 @@
 #include "binary_trees.h"
 
 /**
- * bst_search - searches for a value in BST
+ * bst_search - Function that searches for a value in BST
  * @tree: pointer to the root node
  * @value: value to search
+ *
  * Description: 27. BST - Search
- * Return: see below
- * 1. upon success, return pointer to the node
- * 2. upon fail, return NULL
+ * Return: as follows
+ * upon success, return pointer to the node
+ * upon fail, return NULL
  */
 bst_t *bst_search(const bst_t *tree, int value)
 {
