2529e37d3e2e76a2742077370580b9de2614817b
Gloria-Kaloli <gloriakaloli00@gmail.com>
2024-06-03 15:41:32 -0700

    SIZE

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,10 +1,12 @@
 #include "binary_trees.h"
+
 /**
- * binary_tree_size - measure size of binary tree
+ * binary_tree_size - Function that measures size of binary tree
  * @tree: tree to measure
  *
  * Return: size
  */
+
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (tree)
