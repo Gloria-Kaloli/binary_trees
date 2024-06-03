cf716e05383f6678899c219a40cc14a7af939984
Gloria-Kaloli <gloriakaloli00@gmail.com>
2024-06-03 15:46:14 -0700

    BALANCE FACTORS

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,10 +1,12 @@
 #include "binary_trees.h"
+
 /**
- * binary_tree_nodes - count non-leaf nodes
+ * binary_tree_nodes - Funcyin that count non-leaf nodes
  * @tree: pointer to root
  *
  * Return: count
  */
+
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	if (tree && (tree->left || tree->right))
