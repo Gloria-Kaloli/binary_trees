3353921ba5f457904d6aa6436f57dbdf170a5c9b
Gloria-Kaloli <gloriakaloli00@gmail.com>
2024-06-03 14:47:36 -0700

    PREORDER TRASVERSAL

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,11 +1,12 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - pre-order traversal function
+ * binary_tree_preorder - activate pre-order traversal function
  * @tree: pointer to the root node
  * @func: pointer to function to call for each node
+ *
  * Description: 6. Pre-order traversal
- * Return: nothing
+ * Return: successful
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
