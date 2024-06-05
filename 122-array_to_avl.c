988825d446a42c7c2896460fda56da5fc26a8268
Gloria-Kaloli <gloriakaloli00@gmail.com>
2024-06-05 14:25:22 -0700

    AVL-ARRAY

--- 122-array_to_avl.c
+++ 122-array_to_avl.c
@@ -1,12 +1,11 @@
 #include "binary_trees.h"
 
 /**
- * array_to_avl - Function that turns an array to a avl tree
+ * array_to_avl - turns an array to a avl tree
  * @array: array to turns to AVL tree
  * @size: size of array
  * Return: AVL tree from array
  */
-
 avl_t *array_to_avl(int *array, size_t size)
 {
 	size_t i, j = 0;
